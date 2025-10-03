589acf0f93b698cfe83964927ebfb0155a5d8a3a
atreides <atreides1401@gmail.com>
2025-10-04 00:27:24 +0300

    init

---
+++ MyVector.h
@@ -0,0 +1,49 @@
+#ifndef MYVECTOR_H
+#define MYVECTOR_H
+#include <initializer_list>
+
+class MyVector {
+    int size = 0;
+    int capacity = 0;
+    int grow = 1;
+    int* arr = nullptr;
+public:
+    MyVector(int size);
+    MyVector(std::initializer_list<int> init_arr);
+    MyVector(const int* init_arr, const int& size);
+    MyVector(const MyVector& other);
+    MyVector(MyVector&& other);
+    ~MyVector() { delete[] arr; }
+    int GetSize() const { return size; }
+    void Append(const MyVector& other);
+    void RemoveAll();
+    bool IsEmpty() const;
+    int GetAt(int index) const;
+    void SetAt(const int index, const int value);
+    void GetData() const;
+    void FillArr();
+    void FillArr(const int min_range, const int max_range);
+    void FillArr(const int max_range);
+    static int GetRandNum();
+    static int GetRandNum(const int min_range, const int max_range);
+    static int GetRandNum(const int max_range);
+    void SortByIncreasing();
+    void SortByDecreasing();
+
+    int& operator[](int index) {
+        if (index >= 0 && index < GetSize()) return arr[index];
+        else return arr[0];
+    }
+    const int& operator[](int index) const {
+        if (index >= 0 && index < GetSize()) return arr[index];
+        else return arr[0];
+    }
+    MyVector& operator=(const MyVector& other);
+    MyVector operator+(const MyVector& other) const;
+    void SetSize(int new_size, int new_grow);
+    void PushBack(int value);
+    void Insert(int index, int value);
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "MyVector.h"
+
+int main() {
+    srand(time(nullptr));
+
+    MyVector vector({34, 5, 3, 53});
+    vector.SetSize(15, 4);
+    vector.PushBack(344);
+    vector.PushBack(543);
+    vector.Insert(3, 9999);
+    vector.Insert(16, 342);
+    vector.GetData();
+    // MyVector vector2{{43,13 ,1 ,4 }};
+    // MyVector copy(vector);
+    // MyVector test = vector + vector2;
+    // vector.Append(vector2);
+    // vector.SetAt(1, 367);
+    // std::cout << vector.GetAt(1) << std::endl;
+    // vector.GetData();
+    // MyVector copy2(10);
+    // copy2.FillArr(10);
+    // copy2.SortByIncreasing();
+    // copy2.GetData();
+    // vector.SortByIncreasing();
+    // vector.GetData();
+
+    return 0;
+}
